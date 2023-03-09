be3e8284eb4307f95abcbf2942d06b238406706b
Bloople <galloc1@outlook.com>
2023-03-09 20:03:42 +1030

    Added player prefab, fixed compiler errors with movement script, made TestScene

--- raycast-game/Assets/Movement.cs
+++ raycast-game/Assets/Movement.cs
@@ -4,9 +4,6 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-
-    Rigidbody m_Rigidbody;
-    public float m_Thrust = 20f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,25 +13,5 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            m_Rigidbody.AddForce(transform.up * m_Thrust);
-        }
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            m_Rigidbody.AddForce(transform.left * m_Thrust);
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            m_Rigidbody.AddForce(transform.right * m_Thrust);
-        }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            m_Rigidbody.AddForce(transform.back * m_Thrust);
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            m_Rigidbody.AddForce(transform.forward * m_Thrust);
-        }
     }
 }
